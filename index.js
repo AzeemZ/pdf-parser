11a4d170c373d2409f13126e9428d33379003f86
Azeem Zulfiqar <azeem3z123@gmail.com>
2023-11-27 05:54:18 +0000

    code updated

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
 const fs = require("fs");
 const pdf = require("pdf-parse");
 
-let dataBuffer = fs.readFileSync("pdf-file.pdf");
-
-pdf(dataBuffer).then(function (data) {
-  try {
-    fs.rmSync("parsed-data.csv");
-  } catch (err) {
-    console.error("File does not exit!");
+const pdfFiles = fs.readdirSync("./").filter(filename => {
+  if (filename.split(".")[1] === "pdf") {
+    return filename;
   }
-
-  const dataArr = data.text.split("\n");
-
-  dataArr.forEach((value, index) => {
-    if (value.includes("CASH DEPOSIT")) {
-      fs.appendFileSync(
-        "parsed-data.csv",
-        `${value.replace("CASH DEPOSIT", "")}, ${dataArr[index - 3]
-          .replace(",", "")
-          .replace(".00", "")}\n`
-      );
-    }
-
-    if (
-      value.includes("FUND TRANSFER") &&
-      !value.replace("FUND TRANSFER", "").includes(".00")
-    ) {
-      fs.appendFileSync(
-        "parsed-data.csv",
-        `${value.replace("FUND TRANSFER", "")}, ${dataArr[index - 3]
-          .replace(",", "")
-          .replace(".00", "")}\n`
-      );
-    }
-  });
 });
+
+pdfFiles.forEach(filename => {
+  extractData(filename);
+  fs.rmSync(filename);
+})
+
+function extractData(filename) {
+  let dataBuffer = fs.readFileSync(filename);
+
+  pdf(dataBuffer).then(function (data) {
+    const dataArr = data.text.split("\n");
+
+    dataArr.forEach((value, index) => {
+      if (value.includes("CASH DEPOSIT")) {
+        const splitValues = value.split(" ")
+
+        fs.appendFileSync(
+          filename.replace(".pdf", ".csv"),
+          `${splitValues[1].replace("DEPOSIT", "")}, ${dataArr[index - 3]
+            .replace(",", "")
+            .replace(".00", "")}\n`
+        );
+      }
+
+      if (
+        value.includes("FUND TRANSFER") &&
+        !value.replace("FUND TRANSFER", "").includes(".00")
+      ) {
+        const splitValues = value.split(" ").filter(value => value !== "")
+        
+        fs.appendFileSync(
+          filename.replace(".pdf", ".csv"),
+          `${splitValues[0].replace("FUND", "")}, ${dataArr[index - 3]
+            .replace(",", "")
+            .replace(".00", "")}\n`
+        );
+      }
+    });
+  });
+}
